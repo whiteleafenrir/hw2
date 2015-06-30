829aad6408f2c8149202cf3c530645b9ed3a4939
Artem Aleksandrov <fenrir.90@mail.ru>
2015-06-30 18:16:48 +0300

    general htm/styles/js

--- app/js/common.js
+++ app/js/common.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
 			list=$this.closest('.sidebar__list'),
 			item=$this.closest('.sidebar__item'),
 			items=list.find('.sidebar__item'),
+			triggers=list.find('.sidebar__trigger'),
 			content = item.find('.sidebar__inner'),
 			otherContent = list.find('.sidebar__inner');
 			duration = 300;
@@ -12,12 +13,15 @@ $(document).ready(function(){
 			if(!item.hasClass('active')){
 			
 				items.removeClass('active');
+				triggers.removeClass('sidebar__trigger-active');
 				item.addClass('active');
+				$this.addClass('sidebar__trigger-active');
 				otherContent.stop(true, true).slideUp(duration);
 				content.stop(true, true).slideDown(duration);
 			} else {
 				content.stop(true,true).slideUp(duration);
 				items.removeClass('active');
+				triggers.removeClass('sidebar__trigger-active');
 			}
 	});
 });
