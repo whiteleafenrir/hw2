10c6a6dfd9de0f2dcc0e4975041745c24fe82225
Artem Aleksandrov <fenrir.90@mail.ru>
2015-07-01 18:39:31 +0300

    first final version

--- app/js/common.js
+++ app/js/common.js
@@ -1,7 +1,41 @@
-$(document).ready(function(){
-	$('.sidebar__trigger').on('click',function(e){
-		e.preventDefault();
-		var $this=$(this),
+;var upperModule = (function (){
+	var init = function(){
+		_goUp();
+		_scroll();
+	},
+	upper=$('.upper-button'),
+	_scroll = function(){
+		$(window).on("scroll",_scrollCondition);
+	},
+	_scrollCondition = function(){
+		var scroll=$(document).scrollTop();
+		if(scroll>200){
+			upper.fadeIn();
+			
+		} else {
+			upper.fadeOut();
+		}
+	},
+	_goUp = function(){
+		upper.on('click',function(){
+			$("html, body").animate({scrollTop:0});
+		});
+	};
+		
+	return {
+		init: init
+	};
+
+})();
+upperModule.init();
+;var accardeon = (function (){
+	var init = function(){
+		_clickTrigger();
+	},
+	_clickTrigger = function(){
+		$('.sidebar__trigger').on('click',function(e){
+			e.preventDefault();
+			var $this=$(this),
 			list=$this.closest('.sidebar__list'),
 			item=$this.closest('.sidebar__item'),
 			items=list.find('.sidebar__item'),
@@ -11,7 +45,6 @@ $(document).ready(function(){
 			duration = 300;
 
 			if(!item.hasClass('active')){
-			
 				items.removeClass('active');
 				triggers.removeClass('sidebar__trigger-active');
 				item.addClass('active');
@@ -23,5 +56,13 @@ $(document).ready(function(){
 				items.removeClass('active');
 				triggers.removeClass('sidebar__trigger-active');
 			}
-	});
-});
+		});
+	};
+	
+		
+	return {
+		init: init
+	};
+
+})();
+accardeon.init();
